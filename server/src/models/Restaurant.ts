df0b882aa18b0288e9820a0c6eb8be8a4f7038c2
renatomsa <renatom.sandrade@gmail.com>
2022-05-26 15:25:30 -0300

    changes

--- server/src/models/Restaurant.ts
+++ server/src/models/Restaurant.ts
@@ -1,18 +1,18 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
 @Entity()
-export class User {
+export class Restaurant {
 
     @PrimaryGeneratedColumn()
     id: number
 
     @Column()
-    firstName: string
+    name: string
 
     @Column()
-    lastName: string
+    adress: string
 
     @Column()
-    age: number
+    cuisine: string
 
 }
